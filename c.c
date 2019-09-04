2edf396d1db7d74f4b27c7b0223068b78154a84f
Tin <tuta>
2019-09-04 17:40:29 +0200

    c.c ne dela dobro, 2 puta ide od 0 do (valjda)max

--- c.c
+++ c.c
@@ -2,33 +2,29 @@
 #include <string.h>
 #include <errno.h>
 
+#include <wiringPi.h>
 #include <wiringSerial.h>
 #include <string.h>
-
+#include <pca9685.h>
 #define IBUS_BUFFSIZE 32 // Max iBus packet size (2 byte header, 14 channels x 2 bytes, 2 byte checksum)
-#define IBUS_MAXCHANNELS 10
+#define IBUS_MAXCHANNELS 7
 
-#define THROT_CHANNEL 2
-#define RUDDER_CHANNEL 3
 
-#define LEFT_PWM_PIN 5
-#define RIGHT_PWM_PIN 6
-#define LEFT_DIR_PIN 7
-#define RIGHT_DIR_PIN 8
 
 static int ibusIndex = 0;
 static char ibus[IBUS_BUFFSIZE] = {0};
 static short rcValue[IBUS_MAXCHANNELS];
-
+const int pinBase = 300;
+const int i2caddres = 0x40;
+float freq = 400;
 static int rxFrameDone;
+int fd;
+int fdd ;
+int br;
+int bro;
 
-int ThrotLeft = 0;
-int ThrotRight = 0;
-int DirLeft = 1;
-int DirRight = 1;
 
 
-  int fd ;
 
 
 void readRx()
@@ -37,7 +33,7 @@ void readRx()
 
   if (1)
   {
-    char val = serialGetchar(fd);
+    char val = serialGetchar(fdd);
     // Look for 0x2040 as start of packet
     if (ibusIndex == 0 && val != 0x20) {
       ibusIndex = 0;
@@ -58,10 +54,7 @@ void readRx()
       rcValue[4] = (ibus[11] << 8) + ibus[10];
       rcValue[5] = (ibus[13] << 8) + ibus[12];
       rcValue[6] = (ibus[15] << 8) + ibus[14];
-      rcValue[7] = (ibus[17] << 8) + ibus[16];
-      rcValue[8] = (ibus[19] << 8) + ibus[18];
-      rcValue[9] = (ibus[21] << 8) + ibus[20];
-    }else{
+       }else{
       ibus[ibusIndex] = val;
       ibusIndex++;
     }
@@ -70,10 +63,17 @@ void readRx()
     return;
   }
 }
+
+
 int main ()
 {
-
-  if ((fd = serialOpen ("/dev/ttyAMA0", 115200)) < 0)
+  wiringPiSetup();
+ if((fd = pca9685Setup(300,0x40,500)) < 0)
+ {   printf("Ne dela");
+    return fd;
+}
+pca9685PWMReset(fd);
+  if ((fdd = serialOpen ("/dev/ttyAMA0", 115200)) < 0)
   {
     fprintf (stderr, "Unable to open serial device: %s\n", strerror (errno)) ;
     return 1 ;
@@ -85,8 +85,13 @@ int main ()
   {
 	  readRx();
   if ( rxFrameDone ){
-    printf("%d\n",rcValue[3]);
-    fflush (stdout) ;
-  }
+    printf("%d\n",rcValue[2]);
+    br = (rcValue[2]-1000);
+    bro = (br*0xFFF)/1000;
+    printf("%d \n", bro);
+    fflush(stdout);
+    pca9685PWMWrite(fd,0,(0xFFF-bro),bro);
+    delay(2);
+    }
   }
 }
